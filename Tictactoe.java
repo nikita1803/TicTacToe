f96fd44a784e047ba3e825825284e104b627838f
nikita1803 <rainikita595@gmail.com>
2021-05-18 15:44:42 +0530

    Added: Created a method for board

--- Tictactoe.java
+++ Tictactoe.java
@@ -1,5 +1,19 @@
 package tictactoe;
 
-public class Tictactoe {
-
+public class Tictactoe 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome in Tic tac toe game");
+		char[] board = createBoard();
+	}
+	public static char[] createBoard()
+	{
+		char[] board = new char[10];
+		for(int i = 1; i < board.length; i++)
+		{
+			board[i] = ' ';
+		}
+		return board;
+	}
 }
