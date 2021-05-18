2d1ef72f039055ba1e8ca913903a0104da6adb84
nikita1803 <rainikita595@gmail.com>
2021-05-18 16:52:05 +0530

    Added: Created a method to show the board to the user

--- Tictactoe.java
+++ Tictactoe.java
@@ -9,6 +9,7 @@ public class Tictactoe
 		System.out.println("Welcome in Tic tac toe game");
 		char[] board = createBoard();
 		choose();
+		showBoard(board);
 	}
 	public static char[] createBoard()
 	{
@@ -42,5 +43,14 @@ public class Tictactoe
 			System.out.println("Invalid input please input the valid charcter");
 		}
 		
+	}
+	public static void showBoard(char[] board)
+	{
+		System.out.println(board[1] + " | " + board[2] + " | " + board[3]);
+		System.out.println("--------------");
+		System.out.println(board[4] + " | " + board[5] + " | " + board[6]);
+		System.out.println("--------------");
+		System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
+		System.out.println("--------------");
 	}
 }
