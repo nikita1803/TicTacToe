d21c3c7f12223aa966127dc3781e043aa6eec877
nikita1803 <rainikita595@gmail.com>
2021-05-18 16:30:00 +0530

    Added : Created a method to allow user to choose the character

--- Tictactoe.java
+++ Tictactoe.java
@@ -1,11 +1,14 @@
 package tictactoe;
 
+import java.util.Scanner;
+
 public class Tictactoe 
 {
 	public static void main(String[] args) 
 	{
 		System.out.println("Welcome in Tic tac toe game");
 		char[] board = createBoard();
+		choose();
 	}
 	public static char[] createBoard()
 	{
@@ -15,5 +18,29 @@ public class Tictactoe
 			board[i] = ' ';
 		}
 		return board;
+	}
+	public static void choose()
+	{
+		Scanner sc = new Scanner(System.in);
+		char computerVariable = 0 ;
+		System.out.println("Enter the characater 'X' OR 'O' to play");
+		char playerChoose =  sc. next().charAt(0);
+		if(playerChoose == 'X')
+		{
+			computerVariable = 'O';
+			System.out.println("Player input is :" +playerChoose);
+			System.out.println("Computer input is :" +computerVariable);
+		}
+		else if(playerChoose == 'O')
+		{
+			computerVariable = 'X';
+			System.out.println("Player input is :" +playerChoose);
+			System.out.println("Computer input is :" +computerVariable);
+		}
+		else
+		{
+			System.out.println("Invalid input please input the valid charcter");
+		}
+		
 	}
 }
